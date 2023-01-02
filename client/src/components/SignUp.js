36c6e1c01bcb9f1f23b04868ed258bff493969d3
AGSTRANGER <hannibaletunisien@gmail.com>
2023-01-02 10:22:57 +0100

    Created signup component

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,5 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
-export default function SignUp() {
-  return <div>SignUp</div>;
-}
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Implement sign-up logic here
+  };
+
+  return (
+    <Container className="mx-auto">
+      <Row style={{ borderStyle: "dotted" }}>
+        <Col sm="6" md="3">
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Enter a password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="passwordConfirmation">Confirm Password</Label>
+              <Input
+                type="password"
+                name="passwordConfirmation"
+                id="passwordConfirmation"
+                placeholder="Confirm your password"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+              />
+            </FormGroup>
+            <Button type="submit">Sign Up</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignUp;
