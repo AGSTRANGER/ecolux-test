1f0637d2f9486328277fc0943ac2aac1c7fc3b7f
AGSTRANGER <hannibaletunisien@gmail.com>
2023-01-02 10:08:37 +0100

    Initiated basic routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Regions from "./components/Regions";
+import Products from "./components/Products";
+import Orders from "./components/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Provider store={store}>
+    <Router>
+      {/* <App> */}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/regions" element={<Regions />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+
+      {/* </App> */}
+    </Router>
+    // </Provider>
   );
 }
 
