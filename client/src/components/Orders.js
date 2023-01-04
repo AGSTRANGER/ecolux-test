37104be066a928118448c90c3b30553a490f5f2a
AGSTRANGER <hannibaletunisien@gmail.com>
2023-01-04 13:21:55 +0100

    Displaying orders

--- client/src/components/Orders.js
+++ client/src/components/Orders.js
@@ -1,32 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  ListGroup,
+  ListGroupItem,
+} from "reactstrap";
 
 const Orders = ({ orders }) => {
-  console.log("🚀 ~ file: Orders.js:6 ~ Orders ~ orders", orders);
+  console.log("🚀 ~ file: Orders.js:16 ~ Orders ~ orders", orders);
   return (
-    <Container>
-      <Row>
-        <Col>
-          {orders.map((order, index) => (
-            <ListGroup key={index}>
-              <ListGroupItem>
-                <h3>Order {index + 1}</h3>
-                {order.items.map((item) => (
-                  <div key={item._id}>
-                    <p>{item.title}</p>
-                    <p>{item.quantity}</p>
-                  </div>
-                ))}
-              </ListGroupItem>
+    <div>
+      {orders.map((order) => (
+        <Card key={order._id}>
+          <CardBody>
+            <CardTitle>Order Information</CardTitle>
+            <CardText>Shipping Address: {order.shipping_address}</CardText>
+            <CardText>Order Total: {order.order_total}</CardText>
+            <CardTitle>Items</CardTitle>
+            <ListGroup>
+              {order.items.map((item) => (
+                <ListGroupItem key={item.product}>
+                  <CardText>Product: {item.product.title}</CardText>
+                  <CardText>Price: {item.price}</CardText>
+                  <CardText>Quantity: {item.quantity}</CardText>
+                </ListGroupItem>
+              ))}
             </ListGroup>
-          ))}
-        </Col>
-      </Row>
-    </Container>
+            <CardText>State: {order.state}</CardText>
+            <CardText>Created At: {order.createdAt}</CardText>
+            <CardText>Updated At: {order.updatedAt}</CardText>
+          </CardBody>
+        </Card>
+      ))}
+    </div>
   );
 };
-
 const mapStateToProps = (state) => ({
   orders: state.order.orders,
 });
